46c51f6e833853986b5e0e35507aa59f7c3c0b23
Leo <leo.jasper@studerande.plushogskolan.se>
2025-02-18 11:16:29 +0100

    removed irrelevant classes

--- src/main/java/se/java/security/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package se.java.security.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import se.java.security.models.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
