f0bf96a6de130747744d6b6c89b2d6ac9d3b1531
simonkod2021 <simon98larsson@gmail.com>
2025-02-21 17:40:05 +0100

    Added ReportRepository

---
+++ src/main/java/se/java/security/repository/ReportRepository.java
@@ -0,0 +1,12 @@
+package se.java.security.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import se.java.security.models.Report;
+
+import java.util.Optional;
+
+@Repository
+public interface ReportRepository extends MongoRepository<Report, String> {
+    Optional<Report> findById(String id);
+}
