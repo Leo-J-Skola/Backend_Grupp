bd72f52ed2395bef9da2b28e70780bbb18b9fa51
Axel Liljendal <liljendalaxel@gmail.com>
2025-03-09 22:50:32 +0100

    created FavoriteService class with constructors and imports

--- src/main/java/se/java/security/services/FavoriteService.java
+++ src/main/java/se/java/security/services/FavoriteService.java
@@ -3,21 +3,28 @@ package se.java.security.services;
 import org.springframework.stereotype.Service;
 import se.java.security.models.Favorite;
 import se.java.security.repository.FavoriteRepository;
+import se.java.security.repository.ListingRepository;
+import se.java.security.repository.UserRepository;
 
 @Service
 public class FavoriteService {
 
     private final FavoriteRepository favoriteRepository;
+    private final UserRepository userRepository;
+    private final ListingRepository listingRepository;
     private final ListingService listingService;
     private final UserService userService;
 
 
-    public FavoriteService(FavoriteRepository favoriteRepository, ListingService listingService, UserService userService) {
+    public FavoriteService(FavoriteRepository favoriteRepository, UserRepository userRepository, ListingRepository listingRepository, ListingService listingService, UserService userService) {
         this.favoriteRepository = favoriteRepository;
+        this.userRepository = userRepository;
+        this.listingRepository = listingRepository;
         this.listingService = listingService;
         this.userService = userService;
     }
 
+    // Checks that the user hasnt already favorited the listing object
     public void createFavorite(Favorite favorite) {
 
     }
