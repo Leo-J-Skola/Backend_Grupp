92b6ded0f4741a43a153243454c7f9fd7def41a2
simonkod2021 <simon98larsson@gmail.com>
2025-09-16 20:02:46 +0200

    1. Removed userId from BookingRequest.java, it is not neccessary to provide it from the client. It is automatically provided from authentication. 2. Changed BookingFactory.java createBooking method, it was not neccessary to have it as static. 3.Updated validateBookingRequestData validation for userId check, it is not checked from BookingRequest anymore, its checked from authentication.
    4. Updated BookingService, it now creates bookings properly with listingId from database, userId from authentication and provides bookingId after it is saved to the database. Auth and validation works properly.

--- src/main/java/se/java/security/factory/BookingFactory.java
+++ src/main/java/se/java/security/factory/BookingFactory.java
@@ -1,20 +1,25 @@
 package se.java.security.factory;
 
+import org.springframework.stereotype.Component;
 import se.java.security.models.Booking;
 import se.java.security.models.Listing;
 import se.java.security.models.Status;
+import se.java.security.repository.BookingRepository;
 
 import java.util.Date;
 
+
+@Component
 public class BookingFactory {
-    public static Booking createBooking(Status status, String userId, Listing listingId, double fee, double totalAmount, Date startDate, Date endDate) {
+
+    public Booking createBooking(Status status, String userId, Listing listing, double fee, double totalAmount, Date startDate, Date endDate) {
 
 
         // Create a new booking object
         Booking booking = new Booking();
         booking.setStatus(status);
         booking.setUserId(userId);
-        booking.setListingId(listingId);
+        booking.setListingId(listing);
         booking.setFee(fee);
         booking.setTotalAmount(totalAmount);
         booking.setStartDate(startDate);
--- src/main/java/se/java/security/services/BookingService.java
+++ src/main/java/se/java/security/services/BookingService.java
@@ -5,49 +5,46 @@ import se.java.security.authentication.AuthenticationService;
 import se.java.security.dto.BookingRequest;
 import se.java.security.dto.BookingResponse;
 import se.java.security.exceptions.ListingNotFoundException;
-import se.java.security.exceptions.UserNotFoundException;
+import se.java.security.exceptions.UnauthorizedException;
+import se.java.security.factory.BookingFactory;
 import se.java.security.models.*;
-
 import se.java.security.repository.BookingRepository;
 import se.java.security.repository.ListingRepository;
-import se.java.security.repository.UserRepository;
-
-
-import java.time.temporal.ChronoUnit;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
+import se.java.security.validation.BookingFieldValidation;
 
 
 @Service
 public class BookingService {
 
-    private final ListingRepository listingRepository;
-    private final UserRepository userRepository;
     private final AuthenticationService authenticationService;
+    private final BookingFieldValidation bookingFieldValidation;
+    private final BookingRepository bookingRepository;
+    private final BookingFactory bookingFactory;
+    private final ListingRepository listingRepository;
 
-    public BookingService(ListingRepository listingRepository, UserRepository userRepository, AuthenticationService authenticationService) {
-        this.listingRepository = listingRepository;
-        this.userRepository = userRepository;
+    public BookingService(AuthenticationService authenticationService, BookingFieldValidation bookingFieldValidation, BookingRepository bookingRepository, BookingFactory bookingFactory, ListingRepository listingRepository) {
         this.authenticationService = authenticationService;
+        this.bookingFieldValidation = bookingFieldValidation;
+        this.bookingRepository = bookingRepository;
+        this.bookingFactory = bookingFactory;
+        this.listingRepository = listingRepository;
     }
+
 
     // Try to send a booking request
     public BookingResponse sendBookingRequest(BookingRequest bookingRequest) {
+        // Get user from id from authentication
+        String userId = authenticationService.getCurrentUser().getId();
 
-        // IMPORTANT!
+        bookingFieldValidation.validateBookingRequestData(bookingRequest);
 
-        // Waiting for booking creation from BookingFactory class
-        // Before this method can be completed
-        authenticationService.validateCurrentUser();
-
-
+        // Get listing object from database and use it when creating bookings
         Listing listing = listingRepository.findById(bookingRequest.getListingId())
                 .orElseThrow(() -> new ListingNotFoundException("Listing not found"));
 
-        User user = userRepository.findById(bookingRequest.getUserId())
-                .orElseThrow(() -> new UserNotFoundException("User not found"));
-
-        return null;
+        Booking booking = bookingFactory.createBooking(bookingRequest.getStatus(), userId, listing, bookingRequest.getFee(), bookingRequest.getTotalAmount(), bookingRequest.getStartDate(), bookingRequest.getEndDate());
+        bookingRepository.save(booking);
+        String bookingId = booking.getId();
+        return new BookingResponse(bookingId,"Booking successfully created",bookingRequest.getStatus());
     }
-
 }
