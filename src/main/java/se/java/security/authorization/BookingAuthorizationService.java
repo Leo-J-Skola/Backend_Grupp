56ec5c61d53418bb9fed15e9231824ec8e5dfad8
Leo <leo.jasper@studerande.plushogskolan.se>
2025-09-16 10:14:11 +0200

    Created a new project folder and refactored checkIfBookingOwner method into a new class "BookingAuthorizationService"

---
+++ src/main/java/se/java/security/authorization/BookingAuthorizationService.java
@@ -0,0 +1,17 @@
+package se.java.security.authorization;
+
+import se.java.security.exceptions.UnauthorizedException;
+import se.java.security.models.User;
+import se.java.security.models.Booking;
+
+
+public class BookingAuthorizationService {
+
+        /* This checks if the user is the owner of
+     the booking before letting him update or delete it */
+    public void checkIfBookingOwner(User user, Booking booking) {
+        if (!booking.getUserId().equals(user.getId())) {
+            throw new UnauthorizedException("User is not the owner of the booking");
+        }
+    }
+}
