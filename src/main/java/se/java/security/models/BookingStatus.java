fa17daba35b0252fcac8c97d4458748e28c22bba
Axel Liljendal <liljendalaxel@gmail.com>
2025-10-09 15:40:31 +0200

    added comments to enums for clarity

--- src/main/java/se/java/security/models/BookingStatus.java
+++ src/main/java/se/java/security/models/BookingStatus.java
@@ -3,7 +3,7 @@ package se.java.security.models;
 public enum BookingStatus {
     AVAILABLE, // Listing can be booked during these dates
     PENDING, // When the request is pending, to prevent double bookings
-    DECLINED,
-    ACCEPTED, // When the object is already booked for those dates
+    DECLINED, // Host has declined booking request
+    ACCEPTED, // Host has accepted booking request
     UNAVAILABLE // Other reasons such as admin removed
 }
