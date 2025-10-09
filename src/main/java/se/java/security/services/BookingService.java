23c37abc390240da06c99a055dd2fe4f0cead7f2
Axel Liljendal <liljendalaxel@gmail.com>
2025-10-09 15:59:26 +0200

    updated BookingStatus with new constants, BOOKED changed to ACCEPTED, added DECLINED

--- src/main/java/se/java/security/services/BookingService.java
+++ src/main/java/se/java/security/services/BookingService.java
@@ -100,7 +100,7 @@ public class BookingService {
 
         List<Booking> existingBookings = bookingRepository.findByListingIdAndBookingStatusIn(
                 listing,
-                List.of(BookingStatus.PENDING, BookingStatus.BOOKED)
+                List.of(BookingStatus.PENDING, BookingStatus.ACCEPTED)
         );
 
         return bookingValidationStrategy.isValid(bookingRequest, existingBookings);
